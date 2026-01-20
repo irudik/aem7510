437fc1805bfdd8001d8168279397918191f2f36b
Ivan <irudik@cornell.edu>
2026-01-20 12:00:40 -0500

    update ln1

---
+++ make-pdf.R
@@ -0,0 +1,10 @@
+# Makes pdf slides for all lectures
+lapply(list.files(path = "lecture-notes", pattern = "\\.html$", full.names = TRUE, recursive = TRUE)[1],
+       function(file) {
+         xaringan::decktape(
+           file,
+           output = paste0(tools::file_path_sans_ext(file), ".pdf"),
+           docker = FALSE
+         )
+       }
+)
