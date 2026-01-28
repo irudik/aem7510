af246afe425cc1c00ce7d4cdc4db0e6e3e5e58c2
Ivan <irudik@cornell.edu>
2026-01-28 10:37:48 -0500

    update 03/04 slides for todd 2026

--- make-pdf.R
+++ make-pdf.R
@@ -1,5 +1,5 @@
 # Makes pdf slides for all lectures
-lapply(list.files(path = "lecture-notes", pattern = "\\.html$", full.names = TRUE, recursive = TRUE)[3],
+lapply(list.files(path = "lecture-notes", pattern = "\\.html$", full.names = TRUE, recursive = TRUE)[4],
        function(file) {
          xaringan::decktape(
            file,
